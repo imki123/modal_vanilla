1063b50b3df173e98074758c21fa88a9fd5fd5b0
kiyoung <popping2606@gmail.com>
2020-10-19 00:11:22 +0900

    add items

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-function openModal(_id, title){
-  const $modal = document.querySelector('.modal')
-  if($modal) {
-    $modal.style.zIndex = 10;
-    const $ok = $modal.querySelector('.ok')
-    const $cancel = $modal.querySelector('.cancel')
-    const $input = $modal.querySelector('.input')
-    const $title = $modal.querySelector('.title')
+function openModal(_id, title) {
+  const $modal = document.querySelector(".modal")
+  if ($modal) {
+    $modal.style.zIndex = 10
+    const $ok = $modal.querySelector(".ok")
+    const $cancel = $modal.querySelector(".cancel")
+    const $input = $modal.querySelector(".input")
+    const $title = $modal.querySelector(".title")
     $title.innerHTML = title
 
     const $text = document.querySelector(`#${_id}`)
-    $input.value = $text.innerHTML.indexOf('입력해주세요') === -1 ? $text.innerHTML : ''
+    $input.value =
+      $text.innerHTML.indexOf("입력해주세요") === -1 ? $text.innerHTML : ""
     $input.focus()
-    $ok.onclick = function(){
-      if($text) $text.innerHTML = $input.value
-      $input.value = ''
-      $modal.style.zIndex = -1;
+    $ok.onclick = function () {
+      if ($text)
+        $text.innerHTML =
+          $input.value === "" ? `${title}을 입력해주세요` : $input.value
+      $input.value = ""
+      $modal.style.zIndex = -1
     }
-    $cancel.onclick = function(){
-      $input.value = ''
-      $modal.style.zIndex = -1;
+    $cancel.onclick = function () {
+      $input.value = ""
+      $modal.style.zIndex = -1
     }
-    $input.onkeypress = function(e){
-      if(e.key === 'Enter'){
+    $input.onkeypress = function (e) {
+      if (e.key === "Enter") {
         $ok.click()
         $input.blur()
       }
@@ -29,11 +32,15 @@ function openModal(_id, title){
   }
 }
 
-window.onload = function(){
-  const $profileButton = document.querySelector('.profile .button')
-  $profileButton.onclick = function(){
-    let _id = $profileButton.parentNode.querySelector('.text').id
-    let title = $profileButton.parentNode.querySelector('.title').innerHTML
-    openModal(_id, title)
-  }
+window.onload = function () {
+  const $profileButtons = document.querySelectorAll(".profile .button")
+
+  $profileButtons.forEach(
+    (i) =>
+      (i.onclick = function () {
+        let _id = i.parentNode.querySelector(".text").id
+        let title = i.parentNode.querySelector(".title").innerHTML
+        openModal(_id, title)
+      })
+  )
 }
